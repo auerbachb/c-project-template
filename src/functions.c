e82d818526e2b0f84e6d38516777a08d01228f88
auerbacb <bretton.auerbach@gmail.com>
2016-10-02 08:05:43 -0700

    add dir structure, demo files and makefile for basic c proj making

---
+++ src/functions.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <header.h>
+
+void printHi(void) {
+  printf("Hello makefiles!\n");
+  return;
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <header.h>
+
+int main() {
+    // call a function in another file
+    printHi();
+    return(0);
+}
