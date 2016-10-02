39f8328a6d45a09f37121e8d47aaf5fbb61a4d9b
auerbacb <bretton.auerbach@gmail.com>
2016-10-02 08:16:59 -0700

    check that stdbool can be loaded --> c99 is valid

--- src/functions.c
+++ src/functions.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <header.h>
+#include <stdbool.h>
 
 void printHi(void) {
+  bool myBool = true;
+  if (myBool) printf("stdbool is loaded and working\n"); 
   printf("Hello makefiles!\n");
   return;
 }
